7698e065d5b545e5cca8b776d58ef60eccfcd8f2
Tyko <tykovv@gmail.com>
2022-02-07 16:35:35 -0500

    guessing words okay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use itertools::izip;
+use std::io;
+
 fn load_words(file: String) -> Vec<String> {
     let mut words = vec![];
     let mut rdr = csv::Reader::from_path(file).unwrap();
@@ -67,6 +70,7 @@ enum PosInfo {
 struct KnownInfo {
     pos_info: Vec<PosInfo>,
     not_contains: Vec<char>,
+    contains: Vec<char>,
 }
 
 impl KnownInfo {
@@ -74,11 +78,12 @@ impl KnownInfo {
         KnownInfo {
             pos_info: vec![PosInfo::None; 5],
             not_contains: vec![],
+            contains: vec![],
         }
     }
 }
 
-fn get_possible_words(info: KnownInfo, words: &Vec<String>) -> Vec<String> {
+fn get_possible_words(info: &KnownInfo, words: &Vec<String>) -> Vec<String> {
     let mut pos_words = vec![];
     'word_loop: for word in words {
         for (i, c) in word.chars().enumerate() {
@@ -99,13 +104,88 @@ fn get_possible_words(info: KnownInfo, words: &Vec<String>) -> Vec<String> {
                 }
             }
         }
+        if !info.contains.iter().all(|c| word.contains(*c)) {
+            continue 'word_loop;
+        }
         pos_words.push(word.clone());
     }
     pos_words
 }
 
+fn get_best_word(words: &Vec<String>, pos_freqs: &Vec<LetterFreq>) -> String {
+    let mut scores: Vec<usize> = vec![];
+    for word in words {
+        let mut score: usize = 0;
+        for (pos, c) in word.chars().enumerate() {
+            score += pos_freqs[pos].get_count(c);
+        }
+        scores.push(score);
+    }
+
+    let mut max_i = 0;
+    let mut max_val = 0;
+    for (i, val) in scores.iter().enumerate() {
+        if *val > max_val {
+            max_val = *val;
+            max_i = i;
+        }
+    }
+    words[max_i].clone()
+}
+
+// make a guess and get back the known info from that guess
+fn guess(guess: &String, target: &String, info: &mut KnownInfo) {
+    for (guess_char, target_char, pos_info) in
+        izip!(guess.chars(), target.chars(), info.pos_info.iter_mut())
+    {
+        if target.contains(guess_char) {
+            // TODO ensure no dups
+            info.contains.push(guess_char);
+            if guess_char == target_char {
+                *pos_info = PosInfo::Is(guess_char);
+            } else {
+                match pos_info {
+                    PosInfo::None => *pos_info = PosInfo::Not(vec![guess_char]),
+                    PosInfo::Not(not_char) => not_char.push(guess_char),
+                    _ => panic!("invalid pos_info"),
+                }
+            }
+        } else {
+            // TODO ensure no dups
+            info.not_contains.push(guess_char);
+        }
+    }
+}
+
 fn main() {
-    let words = load_words("5_letters.csv".to_string());
+    let mut words = load_words("5_letters.csv".to_string());
+    let pos_freqs = get_letter_freqs(&words);
+
+    // let mut info = KnownInfo::new();
+    // info.pos_info = vec![
+    //     PosInfo::None,
+    //     PosInfo::None,
+    //     PosInfo::None,
+    //     PosInfo::Is('e'),
+    //     PosInfo::None,
+    // ];
+    // info.not_contains = vec!['s', 'a', 'm', 'y'];
+    // let words = get_possible_words(info, &words);
+    // println!("pos_words: {:?}", words);
+
+    let mut info = KnownInfo::new();
+    let target_word = "primp".to_string();
+    println!("target is: {}", target_word);
+    loop {
+        let best_word = get_best_word(&words, &pos_freqs);
+        println!("guessing: {}", best_word);
+        if best_word == target_word {
+            break;
+        } else {
+            guess(&best_word, &target_word, &mut info);
+            words = get_possible_words(&info, &words);
+        }
+    }
 
     println!("done");
 }
@@ -115,29 +195,6 @@ mod tests {
     #[test]
     fn test_scoring() {
         use crate::*;
-        let words = load_words("5_letters.csv".to_string());
-        let pos_freqs = get_letter_freqs(&words);
-        let mut scores: Vec<usize> = vec![];
-
-        for word in words.iter() {
-            let mut score: usize = 0;
-
-            for (pos, c) in word.chars().enumerate() {
-                score += pos_freqs[pos].get_count(c);
-            }
-
-            scores.push(score);
-        }
-
-        let mut max_i = 0;
-        let mut max_val = 0;
-        for (i, val) in scores.iter().enumerate() {
-            if *val > max_val {
-                max_val = *val;
-                max_i = i;
-            }
-        }
-        println!("best score {} with word {}", max_val, words[max_i]);
     }
     #[test]
     fn get_possible_words() {
