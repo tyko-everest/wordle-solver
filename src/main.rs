5c8f34a3bc495147a14e62144e1cc644af316c33
Tyko <tykovv@gmail.com>
2022-02-07 22:24:01 -0500

    guessing broken into function

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use itertools::izip;
-use std::io;
+use std::io::{self, BufRead, Write};
+use std::vec;
 
-fn load_words(file: String) -> Vec<String> {
+fn load_words(file: String) -> (Vec<String>, Vec<f32>) {
     let mut words = vec![];
+    let mut freqs = vec![];
     let mut rdr = csv::Reader::from_path(file).unwrap();
     for res in rdr.records() {
         let record = res.unwrap();
         let word = record.get(0).unwrap();
+        let freq: f32 = record.get(1).unwrap().parse().unwrap();
         words.push(word.to_string());
+        freqs.push(freq);
     }
-    words
+    (words, freqs)
 }
 
 #[derive(Clone)]
@@ -56,8 +60,10 @@ fn get_letter_freqs(words: &Vec<String>) -> Vec<LetterFreq> {
     ret
 }
 
+#[derive(Clone)]
 struct WordInfo {
     word: String,
+    score: usize,
 }
 
 #[derive(Clone)]
@@ -83,10 +89,10 @@ impl KnownInfo {
     }
 }
 
-fn get_possible_words(info: &KnownInfo, words: &Vec<String>) -> Vec<String> {
-    let mut pos_words = vec![];
-    'word_loop: for word in words {
-        for (i, c) in word.chars().enumerate() {
+fn get_possible_words(info: &KnownInfo, words: &Vec<WordInfo>) -> Vec<WordInfo> {
+    let mut pos_word_infos = vec![];
+    'word_loop: for word_info in words {
+        for (i, c) in word_info.word.chars().enumerate() {
             if info.not_contains.contains(&c) {
                 continue 'word_loop;
             }
@@ -104,37 +110,46 @@ fn get_possible_words(info: &KnownInfo, words: &Vec<String>) -> Vec<String> {
                 }
             }
         }
-        if !info.contains.iter().all(|c| word.contains(*c)) {
+        if !info.contains.iter().all(|c| word_info.word.contains(*c)) {
             continue 'word_loop;
         }
-        pos_words.push(word.clone());
+        pos_word_infos.push((*word_info).clone());
     }
-    pos_words
+    pos_word_infos
 }
 
-fn get_best_word(words: &Vec<String>, pos_freqs: &Vec<LetterFreq>) -> String {
+fn get_scores(
+    words: &Vec<String>,
+    pos_freqs: &Vec<LetterFreq>,
+    word_freqs: &Vec<f32>,
+) -> Vec<usize> {
     let mut scores: Vec<usize> = vec![];
-    for word in words {
+    for (word, word_freq) in izip!(words, word_freqs) {
         let mut score: usize = 0;
         for (pos, c) in word.chars().enumerate() {
             score += pos_freqs[pos].get_count(c);
         }
-        scores.push(score);
+        scores.push(score * ((*word_freq * 1000.0) as usize));
     }
+    scores
+}
 
-    let mut max_i = 0;
-    let mut max_val = 0;
-    for (i, val) in scores.iter().enumerate() {
-        if *val > max_val {
-            max_val = *val;
-            max_i = i;
+fn get_best_word(word_infos: &mut Vec<WordInfo>) -> String {
+    let mut best_word = WordInfo {
+        word: String::new(),
+        score: 0,
+    };
+    for word_info in word_infos {
+        if word_info.score > best_word.score {
+            best_word.word = word_info.word.clone();
+            best_word.score = word_info.score;
         }
     }
-    words[max_i].clone()
+    best_word.word.clone()
 }
 
 // make a guess and get back the known info from that guess
-fn guess(guess: &String, target: &String, info: &mut KnownInfo) {
+fn make_guess(guess: &String, target: &String, info: &mut KnownInfo) {
     for (guess_char, target_char, pos_info) in
         izip!(guess.chars(), target.chars(), info.pos_info.iter_mut())
     {
@@ -157,37 +172,58 @@ fn guess(guess: &String, target: &String, info: &mut KnownInfo) {
     }
 }
 
-fn main() {
-    let mut words = load_words("5_letters.csv".to_string());
-    let pos_freqs = get_letter_freqs(&words);
-
-    // let mut info = KnownInfo::new();
-    // info.pos_info = vec![
-    //     PosInfo::None,
-    //     PosInfo::None,
-    //     PosInfo::None,
-    //     PosInfo::Is('e'),
-    //     PosInfo::None,
-    // ];
-    // info.not_contains = vec!['s', 'a', 'm', 'y'];
-    // let words = get_possible_words(info, &words);
-    // println!("pos_words: {:?}", words);
-
+fn guess(target_word: &String, words: &Vec<String>, scores: &Vec<usize>) {
+    let mut word_infos: Vec<WordInfo> = vec![];
+    for (word, score) in izip!(words, scores) {
+        word_infos.push(WordInfo {
+            word: word.clone(),
+            score: *score,
+        });
+    }
     let mut info = KnownInfo::new();
-    let target_word = "primp".to_string();
-    println!("target is: {}", target_word);
+    if target_word.len() != 5 {
+        println!("not 5 letter word, try again");
+        return;
+    }
+    if !words.contains(&target_word) {
+        println!("word not in dictionary, try again");
+        return;
+    }
     loop {
-        let best_word = get_best_word(&words, &pos_freqs);
+        let best_word = get_best_word(&mut word_infos);
         println!("guessing: {}", best_word);
-        if best_word == target_word {
+        if best_word == *target_word {
             break;
         } else {
-            guess(&best_word, &target_word, &mut info);
-            words = get_possible_words(&info, &words);
+            make_guess(&best_word, &target_word, &mut info);
+            word_infos = get_possible_words(&info, &word_infos);
         }
     }
+}
 
-    println!("done");
+fn main() {
+    let (words, word_freqs) = load_words("5_letters.csv".to_string());
+    let pos_freqs = get_letter_freqs(&words);
+    let scores = get_scores(&words, &pos_freqs, &word_freqs);
+    let mut full_word_infos: Vec<WordInfo> = vec![];
+    for (word, score) in izip!(&words, &scores) {
+        full_word_infos.push(WordInfo {
+            word: word.clone(),
+            score: *score,
+        });
+    }
+
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    loop {
+        let mut target_word = String::new();
+        print!("word to guess: ");
+        stdout.flush().unwrap();
+        stdin.lock().read_line(&mut target_word).unwrap();
+        target_word = target_word.trim().to_string();
+        guess(&target_word, &words, &scores);
+    }
 }
 
 #[cfg(test)]
